eaf6ba86317b9f593449fac076d5e15209ac6be3
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-01-29 15:28:30 +1100

    added printing shortest path

--- Udacity/search.py
+++ Udacity/search.py
@@ -17,6 +17,26 @@
 #   0 = Navigable space
 #   1 = Occupied space
 
+# -----------
+# User Instructions:
+#
+# Modify the the search function so that it returns
+# a shortest path as follows:
+# 
+# [['>', 'v', ' ', ' ', ' ', ' '],
+#  [' ', '>', '>', '>', '>', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', '*']]
+#
+# Where '>', '<', '^', and 'v' refer to right, left, 
+# up, and down motions. Note that the 'v' should be 
+# lowercase. '*' should mark the goal cell.
+#
+# You may assume that all test cases for this function
+# will have a path from init to goal.
+# ----------
+
 grid = [[0, 0, 1, 0, 0, 0],
         [0, 0, 1, 0, 0, 0],
         [0, 0, 0, 0, 1, 0],
@@ -43,7 +63,9 @@ def search(grid,init,goal,cost):
     y = init[1]
     g = 0
     expandCounter = 0
-    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+    expand = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+    path = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
 
     expand[init[0]][init[1]] = expandCounter
     open_list = [[g,x,y]]
@@ -63,33 +85,50 @@ def search(grid,init,goal,cost):
             print 'fail'
         else:
             #remove node with lowest cost
+            #print 'open list: '
+            #print open_list
             open_list.sort()
             open_list.reverse() #to get smallest node cost
             nextNode = open_list.pop()
+            #print 'current node: '
+            #print nextNode
             x = nextNode[1]
             y = nextNode[2]
             g = nextNode[0]
             expand[x][y] = expandCounter
-            expandCounter += 1
+            #expand[x][y] = ' '
+            expandCounter += 1             
 
             #check to see if at goal
             if (x == goal[0] and y == goal[1]):
                 found = True
                 print 'Found Goal state'
                 print nextNode
+                expand[x][y] =  '*'
             else:
                 #all four moves at once
                 for i in range(len(delta)):
                     x2 = x + delta[i][0]
                     y2 = y + delta[i][1]
                     if (x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0])):
+                        #if not checked before and not obstacle
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            open_list.append([g2,x2,y2])
+                            g2 = g + cost                            
+                            open_list.append([g2,x2,y2])                            
                             closed[x2][y2] = 1 #don't search on this node again
-    return expand
+                            action[x2][y2] = i #link chosen action to successor state
 
-expand = search(grid,init,goal,cost)
-#expansion grid.
-for i in range(len(expand)):
-    print expand[i] #step at which node was expanded
+            x = goal[0]
+            y = goal[1]
+        while (x != init[0] and y != init[1]):
+            x2 = x - delta[action[x][y]][0]
+            y2 = y - delta[action[x][y]][1]
+            path[x2][y2] = delta_name[action[x][y]]
+            x = x2
+            y = y2
+
+        for i in range(len(path)):
+            print path[i]
+
+search(grid,init,goal,cost)
+
