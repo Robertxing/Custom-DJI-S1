51b60c8873b215481a2220f3af20a9e59bd09342
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-02-01 21:06:32 +1100

    started examples

--- RaspberryPiTutorials/PhysicalExamples.py
+++ RaspberryPiTutorials/PhysicalExamples.py
@@ -1,1 +1,15 @@
 #Physical Computing on Raspberry Pi
+
+#turning LED on and off (blink like Arduino)
+
+from gpiozero import LED
+from time import sleep
+led = LED(17)
+
+while True:
+  led.on()
+  sleep(1)
+  led.off()
+  sleep(1)
+  
+  #
