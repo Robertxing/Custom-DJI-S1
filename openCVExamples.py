616b492ce183f38e92285a479eb8cd79c8f79320
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-02-03 22:48:32 +1100

    testing video stream and core operation tutorials

--- openCVExamples.py
+++ openCVExamples.py
@@ -1,14 +1,16 @@
 #introduction to openCV with Raspberry Pi
 
+#testing camera photo
+
 #import packages
-from picamera.array import piRGB
+from picamera.array import piRGBArray
 from picamera import PiCamera
 import time
 import cv2
 
 #initialize camera
 camera = PiCamera()
-raw = piRGB(camera)
+raw = piRGBArray(camera)
 
 time.sleep(0.1)
 
@@ -18,5 +20,13 @@ image = raw.array
 cv2.imshow("ImageTest", image)
 cv2.waitKey(0)
 
-#2d convolution filter
+#testing camera video
+
+
+
+#(1)Core Operations - Basic operations on images
+
+#modifying pixel values
+
+
 
