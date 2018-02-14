05672e0f98b210cc136bf6edaa494b384ac7eae3
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-02-14 13:29:51 +1100

    ideas on robot perception

---
+++ RoverPerception.py
@@ -0,0 +1,12 @@
+#Ideas for robotic perception with PiCamera
+
+'''
+1. Region of interest
+2. Edge detection
+3. Tracking red ball (using differential wheel design)
+4. Threshold and warping to find obstacle free path
+5. ML for image classification (later)
+
+'''
+
+#---Pseudocode---
