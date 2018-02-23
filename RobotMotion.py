3a0a22eb0520cd0e360faabcd1894a460fa25d37
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-02-23 13:08:52 +1100

    open and closed loop
    Testing difference between open and closed loop using IMU. Need to fix up speed proportional control

--- RobotMotion.py
+++ RobotMotion.py
@@ -107,6 +107,91 @@ def main(window):
 
 button.wrapper(main)
 
+#Open and Closed Loop Control (using IMU)
+
+#Open loop control
+'''
+#using MotorClass
+
+from gpiozero import Motor
+from time import sleep
+
+motor1 = Motor(4,14) #Right Motor
+motor2 = Motor(17,27) #Left Motor
+
+#robot goes forward at 0.25 speed
+sleep(5)
+motor1.forward(0.25) 
+motor2.forward(0.25)
+
+#To make robot veer to right. expected disturbance
+sleep(5)
+motor1.forward(0.3) 
+motor2.forward(0.5)
+'''
+
+#Closed loop implementation. Using IMU
+
+from gpiozero import Motor
+from time import sleep
+import cv2
+import smbus
+import math
+
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+
+bus = smbus.SMBus(1)
+address = 0x68 #read from i2cdetect -y 1 print out
+
+#I2C Functions
+def read_byte(adr):
+    return bus.read_byte_data(address,adr)
+
+def read_word(adr):
+    high = bus.read_byte_data(address,adr)
+    low = bus.read_byte_data(address, adr+1)
+    val = (high << 8) + low
+    return val
+
+#For gyroscope
+def read_word_2c(adr):
+    val = read_word(adr)
+    if (val >= 0x8000):
+        return -((5535-val)+1)
+    else:
+        return val
+
+motor1 = Motor(4,14) #Right Motor
+motor2 = Motor(17,27) #Left Motor
+
+sleep(5) #wait 5 seconds before starting
+
+while True:
+
+    #read gyro
+    gyro_zout = read_word_2c(0x47) #z-direction
+
+    #5 frame average filter
+      
+    G = gyro_zout/131
+    #G = (G1 + G2 + G3 + G4 + G5)/5 #average
+
+    #Proportional controller
+    velocityR = 0.25 - (G/35)
+    motor1.forward(velocityR)
+
+    velocityL = 0.25 + (G/35)
+    motor2.forward(velocityL)
+
+    sleep(2)
+    
+    key = cv2.waitKey(5) & 0xFF
+    if key == ord("q"):
+        break
+
+
+
 
 
 
