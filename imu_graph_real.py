2e7a0edca68b372bc3a716b18a5f975233c91534
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-09-01 15:21:46 +1000

    6 DOF array printing in real time

--- imu_graph_real.py
+++ imu_graph_real.py
@@ -13,6 +13,8 @@ fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 xs = []
 ys = []
+ys1 = []
+ys2 = []
 
 #start communication with MPU6050 (in module)
 mpu6050v2.imu_init()
@@ -20,33 +22,30 @@ mpu6050v2.imu_init()
 print (" ~MPU6050 Gyro + Accel~ ")
 
 #i is the frame number
-def animate(i,xs,ys):
+def animate(i,xs,ys,ys1,ys2):
 
-    acc_valueZ = mpu6050v2.read_imu()
+    imu_values = mpu6050v2.read_imu() #array of 6 readings
 
     #xs.append(dt.datetime.now().strftime('%H:%M:%S'))
     xs.append(i)
-    ys.append(acc_valueZ)
+    ys.append(imu_values[0])
+    ys1.append(imu_values[1])
+    ys2.append(imu_values[2])
 
     xs = xs[-20:]    
     ys = ys[-20:]
+    ys1 = ys1[-20:]
+    ys2 = ys2[-20:]
 
     i = i + 1
     
     #draw x and y lists everytime
     ax.clear()
-    ax.plot(xs,ys)
+    ax.plot(xs,ys,xs,ys1,xs,ys2)
     
     #formatting
-    plt.title('IMU Real Time AccZ')
-    plt.ylabel('Accel_z (g)')
+    plt.title('IMU Real Time')
+    plt.ylabel('Accel (X,Y,Z)')
     
-ani = animation.FuncAnimation(fig,animate,fargs=(xs,ys),interval=1000)
+ani = animation.FuncAnimation(fig,animate,fargs=(xs,ys,ys1,ys2),interval=500)
 plt.show()
-
-
-
-
-
-
-
